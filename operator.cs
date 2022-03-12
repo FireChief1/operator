53b29e565a345729dd26bedb1b95f041b9f4aa59
Erkut Ateş <100518509+FireChief1@users.noreply.github.com>
2022-03-12 11:30:38 +0300

    Create operator.cs

---
+++ operator.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve işlemeli atama
+            int x=2;
+            int y=2;
+            y=y+2;
+
+            //mantıksal operator
+            // || && !
+            bool isSuccess = true;
+
+        }
+    }
+}
